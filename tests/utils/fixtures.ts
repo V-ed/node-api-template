80e9b9e83bdf6550eb35874a638c158eb7059d5c
V-ed <guillaumemarcoux@gmail.com>
2021-02-01 18:40:35 -0500

    Switch to using better-sqlite3 driver and fix race issues with fixtures
    This makes installations and tests MUCH faster than before, and eliminates nasty race conditions that were present using the old code for fixtures (entities would be saving after the connection was closed).

--- tests/utils/fixtures.ts
+++ tests/utils/fixtures.ts
@@ -1,31 +1,30 @@
 import * as path from 'path';
-import { Connection, createConnection, getRepository } from 'typeorm';
+import { createConnection, getRepository } from 'typeorm';
 import { Builder, fixturesIterator, Loader, Parser, Resolver } from 'typeorm-fixtures-cli/dist';
 
 export async function loadFixtures(fixturesPath: string): Promise<void> {
-	let connection: Connection | undefined;
+	const connection = await createConnection();
 
-	try {
-		connection = await createConnection();
-		await connection.synchronize(true);
+	await connection.synchronize(true);
 
-		const loader = new Loader();
+	const loader = new Loader();
 
-		loader.load(path.resolve(fixturesPath));
+	loader.load(path.resolve(fixturesPath));
 
-		const resolver = new Resolver();
-		const fixtures = resolver.resolve(loader.fixtureConfigs);
-		const builder = new Builder(connection, new Parser());
+	const resolver = new Resolver();
+	const fixtures = resolver.resolve(loader.fixtureConfigs);
+	const builder = new Builder(connection, new Parser());
+	
+	const entitiesPromiseMap = [];
+	
+	for (const fixture of fixturesIterator(fixtures)) {
+		const entity = await builder.build(fixture);
+		const repo = getRepository(entity.constructor.name);
 
-		for (const fixture of fixturesIterator(fixtures)) {
-			const entity = await builder.build(fixture);
-			const repo = getRepository(entity.constructor.name);
-
-			repo.save(entity);
-		}
-	} finally {
-		if (connection) {
-			await connection.close();
-		}
+		entitiesPromiseMap.push(repo.save(entity));
 	}
+	
+	await Promise.all(entitiesPromiseMap);
+	
+	await connection.close();
 }
