99a5167039aa43990d8ec8486eb243973c022e28
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2021-05-20 11:30:40 -0400

    Disable API and implement GraphQL examples
    This adds queriees, mutations and subscription examples.
    
    A custom solution was made to properly handle subscriptions and mutations using PrismaSelect, by aggregating all subscriptions queries and merging to get the biggest query possible to allow every query to be fulfilled.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
-import { AppGateway } from './app.gateway';
+// import { AppGateway } from './app.gateway';
 import { MessageModule } from './message/message.module';
-import { SocketModule } from './socket/socket.module';
+// import { SocketModule } from './socket/socket.module';
+import { PubSub } from './pub-sub';
+
+const isProd = process.env.NODE_ENV == 'production';
+
+const graphqlModule = GraphQLModule.forRoot({
+	autoSchemaFile: true,
+	installSubscriptionHandlers: true,
+	debug: !isProd,
+	playground: !isProd,
+});
 
 @Module({
-	imports: [SocketModule, MessageModule],
+	imports: [
+		graphqlModule,
+		// SocketModule,
+		MessageModule,
+	],
 	controllers: [AppController],
-	providers: [AppGateway],
+	providers: [
+		// AppGateway
+		PubSub,
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SocketIoAdapter } from './socket/SocketIoAdapter';
+// import { SocketIoAdapter } from './socket/SocketIoAdapter';
 
 const DEFAULT_PORT = 3000;
 
@@ -12,8 +12,8 @@ async function bootstrap(): Promise<void> {
 
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-	// TODO : Remove Socket.IO v3 custom adapter once Nest supports it
-	app.useWebSocketAdapter(new SocketIoAdapter(app));
+	// // TODO : Remove Socket.IO v3 custom adapter once Nest supports it
+	// app.useWebSocketAdapter(new SocketIoAdapter(app));
 
 	await app.listen(PORT);
 
