a2677a6b99801e2f310f29756a128347b7364ac7
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2021-05-25 14:50:23 -0400

    Buff testing suite
    This allows a greater range of flexibility for jest that allows custom setups while extending a base one.
    
    A new "all" config was added to test all configs at once, making tooling more efficient (tools such as vscode-jest can now test the whole test suite instead of only the specs, for example)
    
    GitHub Actions now only init one test step for all the repo. This can be changed to split different tests suites if needed for performance improvement, for example.
    
    All tests are now able to use the `any` keyword, and also use magic numbers, as these are often used for quick testing.
    
    vscode's debug menu was also split by groups, allowing a clearer visualization of the different debugging configurations.

--- .jest/base.ts
+++ .jest/base.ts
@@ -1,23 +1,33 @@
 import type { Config } from '@jest/types';
+import deepmerge from 'deepmerge';
 import fs from 'fs';
 import JSON5 from 'json5';
 import { pathsToModuleNameMapper } from 'ts-jest/utils';
 
 const tsConfig = JSON5.parse(fs.readFileSync('./tsconfig.json', 'utf-8'));
 
-const config: Config.InitialOptions = {
+function defineBaseJestConfig<T extends Config.InitialOptions>(config: T) {
+	return config;
+}
+
+const baseConfig = defineBaseJestConfig({
+	rootDir: '../',
 	testEnvironment: '<rootDir>/prisma/prisma.test-environment.js',
 	moduleFileExtensions: ['js', 'json', 'ts'],
-	// rootDir: 'src',
-	testRegex: '.*\\.spec\\.ts$',
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
 	},
 	testPathIgnorePatterns: ['<rootDir>/dist/'],
 	collectCoverageFrom: ['**/*.ts', '!gulpfile.ts'],
-	coverageDirectory: './coverage',
-	coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/tests/', '<rootDir>/prisma/', '<rootDir>/dist/'],
+	coverageDirectory: '<rootDir>/coverage',
+	coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/prisma/', '<rootDir>/dist/'],
 	moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: '<rootDir>/' }),
-};
+	testTimeout: 30000,
+});
 
-export default config;
+export function defineJestConfig<T extends Config.InitialOptions>(config: T): T & typeof baseConfig {
+	// @ts-ignore
+	return deepmerge(config, baseConfig);
+}
+
+export default baseConfig;
