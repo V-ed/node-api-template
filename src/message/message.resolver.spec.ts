133f5391cd3fd5089a6b4530e7cf7dfe9ed5f201
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2021-05-25 17:34:47 -0400

    Tweak end to end tests to test GraphQL endpoints

--- src/message/message.resolver.spec.ts
+++ src/message/message.resolver.spec.ts
@@ -1,18 +1,20 @@
+import { PrismaService } from '$/prisma.service';
+import { PubSub } from '$/pub-sub';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageResolver } from './message.resolver';
 
 describe('MessageResolver', () => {
-  let resolver: MessageResolver;
+	let resolver: MessageResolver;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MessageResolver],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [MessageResolver, PrismaService, PubSub],
+		}).compile();
 
-    resolver = module.get<MessageResolver>(MessageResolver);
-  });
+		resolver = module.get<MessageResolver>(MessageResolver);
+	});
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(resolver).toBeDefined();
+	});
 });
--- src/pub-sub.spec.ts
+++ src/pub-sub.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PubSub } from './pub-sub';
 
 describe('PubSub', () => {
-  let provider: PubSub;
+	let provider: PubSub;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PubSub],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [PubSub],
+		}).compile();
 
-    provider = module.get<PubSub>(PubSub);
-  });
+		provider = module.get<PubSub>(PubSub);
+	});
 
-  it('should be defined', () => {
-    expect(provider).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(provider).toBeDefined();
+	});
 });
