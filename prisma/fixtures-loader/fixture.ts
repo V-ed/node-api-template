41f909d1a2ba8f9292006640b8b0e8269388b093
V-ed <guillaumemarcoux@gmail.com>
2021-06-19 15:38:42 -0400

    Cleanup unused Fixture properties

--- prisma/fixtures-loader/fixture.ts
+++ prisma/fixtures-loader/fixture.ts
@@ -9,8 +9,6 @@ export type Dependency = ModelDependency<IdentityModel>;
 
 type ModelType<T extends ModelDependency<IdentityModel>> = T extends ModelDependency<infer Model> ? Model : never;
 
-type DepsData<Deps extends Dependency[]> = Record<string, ModelType<Deps[number]>[]>;
-
 export type LinkMethod<F extends Fixture> = (
 	dependency: F['dependencies'][number],
 	_option: number | Range | LinkMode,
@@ -20,31 +18,12 @@ export type LinkMethod<F extends Fixture> = (
 export type Range = { from: number; to: number };
 
 export enum LinkMode {
-	RANDOM,
+	RANDOM = 'RANDOM',
 }
 
 // Fixture class
 export abstract class Fixture<T extends IdentityModel = IdentityModel> {
 	abstract readonly dependencies: Dependency[];
-
-	seedData?: T[];
-	#dependenciesData?: DepsData<this['dependencies']>;
-
-	setDependencyData(data: DepsData<this['dependencies']>) {
-		this.#dependenciesData = data;
-	}
-
-	link(dependency: this['dependencies'][number], _option: number | Range | LinkMode): ModelType<typeof dependency> {
-		const data = this.#dependenciesData?.[dependency.name];
-
-		if (!data) {
-			throw 'Data missing!';
-		}
-
-		const test = data[0];
-
-		return test;
-	}
 
 	abstract seed(prisma: PrismaClient, link: LinkMethod<this>): Promise<T[]>;
 }
