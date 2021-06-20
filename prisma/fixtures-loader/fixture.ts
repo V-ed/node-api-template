c49f6ac1d1d36f57d75158399eef3e3cd76a0d86
V-ed <guillaumemarcoux@gmail.com>
2021-06-19 23:29:31 -0400

    Add jsdoc for link method and link modes

--- prisma/fixtures-loader/fixture.ts
+++ prisma/fixtures-loader/fixture.ts
@@ -11,6 +11,14 @@ type ModelType<T extends ModelDependency<IdentityModel>> = T extends ModelDepend
 
 export type LinkMethod<F extends Fixture> = (
 	dependency: F['dependencies'][number],
+	/**
+	 * The model to fetch from the given fixture dependency.
+	 *
+	 * There are three methods to use this option :
+	 * - `number` : index of fixtures data array;
+	 * - `ranged object {from, to}` : a single index, randomly selected between these two numbers;
+	 * - `LinkMode enum` : special modes for different purposes.
+	 */
 	option: number | Range | LinkMode,
 ) => ModelType<typeof dependency>;
 
@@ -18,6 +26,7 @@ export type LinkMethod<F extends Fixture> = (
 export type Range = { from: number; to: number };
 
 export enum LinkMode {
+	/** Fetch a random model from all the available fixture entries. */
 	RANDOM = 'RANDOM',
 }
 
