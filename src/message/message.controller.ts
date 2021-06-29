9458cd6a2b1102c4ea6569c6f96e3f6976c4e87c
V-ed <guillaumemarcoux@gmail.com>
2021-06-28 20:15:04 -0400

    Remove unused message controller

--- src/message/message.controller.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '$/prisma.service';
-import { Controller } from '@nestjs/common';
-import { MessageService } from './message.service';
-
-@Controller('messages')
-export class MessageController {
-	constructor(private readonly prisma: PrismaService, private readonly messageService: MessageService) {}
-}
