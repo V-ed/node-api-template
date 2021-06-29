56255c39a3dfa0dd3b6284585326293b654a8761
V-ed <guillaumemarcoux@gmail.com>
2021-06-28 20:14:49 -0400

    Fix tests by correctly providing MessageService and dependencies

--- src/message/message.resolver.spec.ts
+++ src/message/message.resolver.spec.ts
@@ -2,13 +2,14 @@ import { PrismaService } from '$/prisma.service';
 import { PubSub } from '$/pub-sub';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageResolver } from './message.resolver';
+import { MessageService } from './message.service';
 
 describe('MessageResolver', () => {
 	let resolver: MessageResolver;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [MessageResolver, PrismaService, PubSub],
+			providers: [MessageResolver, PrismaService, PubSub, MessageService],
 		}).compile();
 
 		resolver = module.get<MessageResolver>(MessageResolver);
--- src/message/message.service.spec.ts
+++ src/message/message.service.spec.ts
@@ -1,4 +1,5 @@
 import { PrismaService } from '$/prisma.service';
+import { PubSub } from '$/pub-sub';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageService } from './message.service';
 
@@ -7,7 +8,7 @@ describe('MessageService', () => {
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [MessageService, PrismaService],
+			providers: [MessageService, PrismaService, PubSub],
 		}).compile();
 
 		service = module.get<MessageService>(MessageService);
