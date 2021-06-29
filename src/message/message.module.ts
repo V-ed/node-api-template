d8f3f7def5c56924687825ed6800cb795e9dd68d
V-ed <guillaumemarcoux@gmail.com>
2021-06-28 20:05:23 -0400

    Move business logic from resolver to service

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,8 +2,9 @@ import { PrismaService } from '$/prisma.service';
 import { PubSub } from '$/pub-sub';
 import { Module } from '@nestjs/common';
 import { MessageResolver } from './message.resolver';
+import { MessageService } from './message.service';
 
 @Module({
-	providers: [PrismaService, MessageResolver, PubSub],
+	providers: [PrismaService, MessageResolver, PubSub, MessageService],
 })
 export class MessageModule {}
--- src/message/message.resolver.ts
+++ src/message/message.resolver.ts
@@ -1,46 +1,29 @@
-import { PrismaService } from '$/prisma.service';
-import { PubSub } from '$/pub-sub';
 import { Message, MessageCreateInput, MessageUpdateWithWhereUniqueWithoutUserInput, MessageWhereInput } from '$prisma-graphql/message';
 import { Args, Info, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
-import { PrismaSelect } from '@paljs/plugins';
 import { GraphQLResolveInfo } from 'graphql';
+import { MessageService } from './message.service';
 
 @Resolver(() => Message)
 export class MessageResolver {
-	constructor(private prismaService: PrismaService, private pubSub: PubSub) {}
+	constructor(private messageService: MessageService) {}
 
 	@Query(() => [Message])
-	async messages(@Args('where', { nullable: true }) where: MessageWhereInput, @Info() info: GraphQLResolveInfo) {
-		const select = new PrismaSelect(info).value;
-
-		const messages = await this.prismaService.message.findMany({ where, ...select });
-
-		return messages;
+	messages(@Args('where', { nullable: true }) where: MessageWhereInput, @Info() info: GraphQLResolveInfo) {
+		return this.messageService.find(info, where);
 	}
 
 	@Mutation(() => Message, { nullable: true })
-	async addMessage(@Args('data') data: MessageCreateInput, @Info() info: GraphQLResolveInfo) {
-		const message = await this.pubSub.prismaMutate('messageAdded', info, (allSelect) => {
-			return this.prismaService.message.create({ data, ...allSelect });
-		});
-
-		return message;
+	addMessage(@Args('data') data: MessageCreateInput, @Info() info: GraphQLResolveInfo) {
+		return this.messageService.create(info, data);
 	}
 
 	@Mutation(() => Message)
-	async updateMessage(@Args('query') query: MessageUpdateWithWhereUniqueWithoutUserInput, @Info() info: GraphQLResolveInfo) {
-		const select = new PrismaSelect(info).value;
-
-		const where = query.where;
-		const data = { time: new Date(), ...query.data };
-
-		const updatedMessage = await this.prismaService.message.update({ where, data, ...select });
-
-		return updatedMessage;
+	updateMessage(@Args('query') query: MessageUpdateWithWhereUniqueWithoutUserInput, @Info() info: GraphQLResolveInfo) {
+		return this.messageService.update(info, query);
 	}
 
 	@Subscription(() => Message, { name: 'messageAdded' })
 	subscribeMessageAdded(@Info() info: GraphQLResolveInfo) {
-		return this.pubSub.prismaSubscribe('messageAdded', info);
+		return this.messageService.subscribeAdded(info);
 	}
 }
