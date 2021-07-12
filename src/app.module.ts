1233030d95e199b27cd3978b19f85401e55acf2c
V-ed <guillaumemarcoux@gmail.com>
2021-07-12 16:27:18 -0400

    Move GraphQLModule in its own file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import depthLimit from 'graphql-depth-limit';
-import { ConfigModule, env } from './config.module';
-import { Environment } from './env.validation';
+import { ConfigModule } from './config.module';
+import { GraphQLModule } from './graphql.module';
 import { MessageModule } from './message/message.module';
 import { PubSub } from './pub-sub';
 
-const isProd = env.NODE_ENV == Environment.Production;
-
-const graphqlModule = GraphQLModule.forRoot({
-	autoSchemaFile: true,
-	installSubscriptionHandlers: true,
-	debug: !isProd,
-	playground: !isProd,
-	validationRules: [depthLimit(env.GRAPHQL_DEPTH_LIMIT)],
-});
-
 @Module({
-	imports: [graphqlModule, ConfigModule, MessageModule],
+	imports: [GraphQLModule, ConfigModule, MessageModule],
 	providers: [PubSub],
 })
 export class AppModule {}
---
+++ src/graphql.module.ts
@@ -0,0 +1,14 @@
+import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
+import depthLimit from 'graphql-depth-limit';
+import { env } from './config.module';
+import { Environment } from './env.validation';
+
+const isProd = env.NODE_ENV == Environment.Production;
+
+export const GraphQLModule = NestGraphQLModule.forRoot({
+	autoSchemaFile: true,
+	installSubscriptionHandlers: true,
+	debug: !isProd,
+	playground: !isProd,
+	validationRules: [depthLimit(env.GRAPHQL_DEPTH_LIMIT)],
+});
