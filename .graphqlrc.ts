76ab3777df6c78c002621d49ab6040922cf21582
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2021-10-04 10:39:20 -0400

    fix: broken import in .graphqlrc.ts file

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -1,5 +1,5 @@
 import type { IGraphQLConfig } from 'graphql-config';
-import { env } from './src/config.module';
+import { env } from './src/@common/configs/config.module';
 
 const config: IGraphQLConfig = {
 	schema: `http://localhost:${env.PORT}/graphql`,
