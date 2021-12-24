399edef8d6f25a13577c7932292ef996b23d1420
V-ed <guillaumemarcoux@gmail.com>
2021-12-24 11:50:47 -0500

    refactor(tests): removes now useless type casting

--- prisma/prisma.test-environment.ts
+++ prisma/prisma.test-environment.ts
@@ -6,7 +6,7 @@ export class PrismaTestEnvironment extends NodeEnvironment {
 		const dbURL = process.env.TEST_DATABASE_URL ?? env.TEST_DATABASE_URL;
 
 		process.env.DATABASE_URL = dbURL;
-		(<any>this.global.process).env.DATABASE_URL = dbURL;
+		this.global.process.env.DATABASE_URL = dbURL;
 
 		return super.setup();
 	}
