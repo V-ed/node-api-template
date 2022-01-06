8e208c9723551e5abc8673713c4b5378e4a75c40
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-01-06 18:36:05 -0500

    refactor(Prisma Seed): tweak cli output to use pretty table

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,11 +4,27 @@ async function runCliSeeds() {
 	const seedData = await seed();
 
 	const formattedData = seedData.map((data) => ({
-		fixture: data.name,
-		modelsCount: data.models.length,
+		Seeder: data.name,
+		'Models Count': data.models.length,
 	}));
 
-	console.log(`Seed data :`, formattedData);
+	const assumedLength = 29;
+
+	const seedersName = seedData.map((data) => data.name);
+	const maxSeederLength = Math.max(...seedersName.map((el) => el.length));
+
+	const spacingStartCount = Math.max(1, Math.floor(maxSeederLength / 2) - 1);
+	const spacingEndCount = Math.max(1, Math.round(maxSeederLength / 2) - 1);
+
+	const spacingStart = ` `.repeat(spacingStartCount);
+	const spacingEnd = ` `.repeat(spacingEndCount);
+
+	const topBar = `─`.repeat(Math.max(assumedLength + 4, assumedLength + maxSeederLength));
+
+	console.log();
+	console.log(`┌${topBar}┐`);
+	console.log(`|${spacingStart}Seed data, ordered by run order${spacingEnd}|`);
+	console.table(formattedData);
 }
 
 runCliSeeds();
